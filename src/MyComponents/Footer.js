847175f91b53c723901f3dcfaf3ecc1789c29bfc
All-Rounder-Tuber <67503641+All-Rounder-Tuber@users.noreply.github.com>
2021-05-16 00:54:56 +0530

    Initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  // let footerStyle = {
+  //   position: "relative",
+  //   top: "5vh",
+  //   width: "100vw",
+  //   border: "2px solid red"
+  // }
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">Copyright &copy; MyTodosList.com</p>
+    </footer>
+  );
+};
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { TodoItem } from "../MyComponents/Todo.js";
+
+export default function Todos(props) {
+  const myStyle = {
+    minHeight : "70vh"
+  }
+
+  return (
+    <div className="container my-5" style={myStyle}>
+      <h3 className="my-3">Todos List</h3>
+      {props.todos.length===0? "No Todos to Display" : 
+        props.todos.map((todo) => {
+        return (<>
+        <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+        <hr/>
+        </>)
+      })
+      }
+      {/* <TodoItem todo={props.todos[1]}/> */}
+    </div>
+  );
+}
