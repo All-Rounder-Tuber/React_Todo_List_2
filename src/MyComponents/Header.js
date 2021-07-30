0aed8e809a60133a279634fc108289516ce4279d
All-Rounder-Tuber <67503641+All-Rounder-Tuber@users.noreply.github.com>
2021-07-30 18:29:23 +0530

    Update Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           {props.title}
